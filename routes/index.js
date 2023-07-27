a9ad9e3866651af2f85b31ab0ff9ad74dcd5581a
gafab <gabofabius@gmail.com>
2023-07-27 19:21:03 -0300

    ,

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
